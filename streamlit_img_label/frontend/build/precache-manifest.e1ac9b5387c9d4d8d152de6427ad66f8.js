3e683fed440124f02fa29c751ce35b17e694e3fb
Basavaraj Sangayya Hiremath <basavaraj.sangayya@ovgu.de>
2024-01-07 17:22:51 +0000

    updated app

---
+++ streamlit_img_label/frontend/build/precache-manifest.e1ac9b5387c9d4d8d152de6427ad66f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1579e9313a6c8a34c635",
+    "url": "./static/css/main.c6eb945a.chunk.css"
+  },
+  {
+    "revision": "1579e9313a6c8a34c635",
+    "url": "./static/js/main.1579e931.chunk.js"
+  },
+  {
+    "revision": "43c9ca97d979d0b0ddc5",
+    "url": "./static/js/1.43c9ca97.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "654cfdee49f6e00de3a8681a924dbd4c",
+    "url": "./index.html"
+  }
+];
